b88851cc719069c49eb10a3c78a312ab1c1bb8f4
newt <newt@tutoring.co.kr>
2021-06-18 14:47:29 +0900

    폰트사이즈 테스트

--- app-cra/src/Router.js
+++ app-cra/src/Router.js
@@ -4,6 +4,7 @@ import LearnConsultReserve from './components/page/mobileapp/LearnConsultReserve
 import LearnConsultReserveResult from './components/page/mobileapp/LearnConsultReserveResult/LearnConsultReserveResult';
 import Home from './components/page/home/Home';
 import Arrive from './components/page/jjiayou/Arrive';
+import Test from './components/test/Test';
 
 function Router() {
   return (
@@ -16,6 +17,8 @@ function Router() {
       <Route exact path="/result" component={LearnConsultReserveResult} />
       {/* 짜요 다왔다 */}
       <Route exact path="/arrive" component={Arrive} />
+      {/* 테스트페이지 */}
+      <Route exact path="/test" component={Test} />
     </>
   );
 }
---
+++ app-cra/src/components/test/Test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Test.scss';
+
+const Test = () => {
+  return (
+    <div className="wrap">
+      <p className="px12">폰트사이즈 12px</p>
+      <p className="px10">폰트사이즈 10px</p>
+      <p className="px8">폰트사이즈 8px</p>
+      <p className="px6">폰트사이즈 6px</p>
+      <p className="px4">폰트사이즈 4px</p>
+      <p className="px3">폰트사이즈 3px</p>
+      <p className="px2">폰트사이즈 2px</p>
+      <p className="px1">폰트사이즈 1px</p>
+    </div>
+  );
+};
+
+export default Test;
