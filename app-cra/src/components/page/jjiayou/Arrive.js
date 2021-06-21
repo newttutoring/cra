ecde91399608bba3cb224aef125ad7f111061189
newt <newt@tutoring.co.kr>
2021-06-21 15:02:03 +0900

    하단 공지사항

--- app-cra/src/components/page/jjiayou/Arrive.js
+++ app-cra/src/components/page/jjiayou/Arrive.js
@@ -50,6 +50,73 @@ const Arrive = () => {
         <section className={cx('img3')}></section>
         <section className={cx('img4')}></section>
 
+
+        <section className={cx("ontact-review")}>
+          <h2 className={cx("heading")}>직접 이용하고 계신<br />회원님들의 솔직한 후기</h2>
+          <div className={cx("review__item")}>
+            <p className={cx("description")}>시간, 장소의 제약 없이 내가 원할 때 영어 공부가 가능해서 좋아요. 실제 업무에서 쓰고 싶은 내용을 수업에서 튜터와 이야기 합니다. 어색한 부분이나 잘못된 문장을 교정해 주실때 도움이 많이 돼요.</p>
+            <span className={cx("name")}>미정님</span>
+          </div>
+          <div className={cx("review__item")}>
+            <p className={cx("description")}>정해진 수업 시간은 나중에 그 시간이 압박으로 다가오는데, 튜터링은 아무 때나 내가 원하는 시간에 수업을 할 수 있고  또 토픽카드로 셀프 스터디를 할 수 있어서 좋아요.</p>
+            <span className={cx("name")}>써니님</span>
+          </div>
+          <div className={cx("review__item")}>
+            <p className={cx("description")}>내가 원하는 시간에 수업이 가능하고, 원하는 시간만큼 연장 수업이 가능한 부분이 편리해요. 또 수업이 끝나면 피드백을 바로 받을 수 있고, 들으며 놓쳤던 부분을 이해할 수 있어 복습에 도움이 됩니다. </p>
+            <span className={cx("name")}>수진님</span>
+          </div>
+        </section>
+
+        <div className={cx("section_notice")}>
+			<div className={cx("certificate")}>
+				<div className={cx("certificate__wrapper")}>
+					<p className={cx("title")}>수강증 상시 발급 가능</p>
+					<div className={cx("certificate_content")}>
+						<p className={cx("certificate_content__description")}>
+							수강증, 출석확인증, 영수증 상시 발급 가능
+						</p>
+						<div className={cx("certificate_content__banner")}>
+							직장인들을 위한 SPEC UP!<br/> 사내 교육지원비로 알뜰하게!
+						</div>
+						<div className={cx('button__wrap')}>
+							<button type="button" className={cx("receipt_detail")} >
+								증빙서류 샘플 상세보기
+								<i className={cx("arrow")} />
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className={cx("guide")}>
+					<div className={cx("guide__item")}>
+						<p className={cx("guide__item__title")}>증빙 서류 발급 안내</p>
+						<ul>
+							<li className={cx("guide__item__description")}>· 수강증, 출석 확인증, 영수증은 튜터링 앱 메인 화면 우측 하단 [마이튜터링] &gt; [고객센터] &gt; [증빙 서류 발급 메뉴]에서 신청 가능합니다.</li>
+							<li className={cx("guide__item__description")}>· 정해진 학습 기간 내에 수업을 진행해야만 출석으로 인정합니다.</li>
+							<li className={cx("guide__item__description")}>· 증빙서류의 경우 내용(수강권명, 금액)등은 임의 수정이 불가합니다.</li>
+							<li className={cx("guide__item__description")}>· 발급 신청 시 입력한 이메일 주소로 자동 발급됩니다. (기타 문의 사항은 [고객센터 &gt; 1:1 채팅 문의]로 연락주시기 바랍니다.)</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div className={cx("section4")}>
+				<div className={cx("notice")}>
+					<div className={cx("notice--header")}>튜터링 영어 유의사항 안내</div>
+				</div>
+				<div className={cx("guide")}>
+							<div className={cx("info-group")}>
+								<p className={cx("info-group__title")}>제목</p>
+								<ul className={cx("info-group__content")}>
+                  <li className={cx("content__item")}>내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용</li>
+                  <li className={cx("content__item")}>내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용</li>
+                  <li className={cx("content__item")}>내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용</li>
+                  <li className={cx("content__item")}>내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용</li>
+								</ul>
+							</div>
+				</div>
+			</div>
+		</div>
+        {/* */}
+
         {/* cta */}
         <section className={cx('challenge-cta', 'is-info')} id="challengeCta">
           <div className={cx('challenge-cta__wrap')}>
@@ -71,7 +138,7 @@ const Arrive = () => {
             <div className={cx('sheet__wrap')}>
               <div className={cx('info')}>
                 <div className={cx('btn')} />
-                  <strong className={cx('option')}>수강권 선택 옵션</strong>
+                <strong className={cx('option')}>수강권 선택 옵션</strong>
                 <div className={cx('change-btn')}>
                   <span className={cx('text')}>변경</span>
                 </div>
