219e3da48973cd611fe572e87b1d633e2466f907
newt <newt@tutoring.co.kr>
2021-06-21 13:27:19 +0900

    cx 문법 수정

--- app-cra/src/components/page/jjiayou/Arrive.js
+++ app-cra/src/components/page/jjiayou/Arrive.js
@@ -20,7 +20,7 @@ const Arrive = () => {
           <div className={cx('textarea')}>
             <div className={cx('textarea__option')}>
               <div className={cx('title')}>365올패스 옵션 선택</div>
-              <div className={cx('tamo-layer l-select is-on')}>
+              <div className={cx('tamo-layer', 'l-select', 'is-on')}>
                 <span className={cx('selected__plan__title')}>
                   <span className={cx('badge-red')}>추천</span>Global 12개월 20분 주 2회
                 </span>
@@ -51,7 +51,7 @@ const Arrive = () => {
         <section className={cx('img4')}></section>
 
         {/* cta */}
-        <section className={cx('challenge-cta is-info')} id="challengeCta">
+        <section className={cx('challenge-cta', 'is-info')} id="challengeCta">
           <div className={cx('challenge-cta__wrap')}>
             <div className={cx('cta-wrapper')}>
               <div className={cx('btnwrap')}>
@@ -59,7 +59,7 @@ const Arrive = () => {
                   <span className={cx('money__per__month')}>40,000</span>원
                   <span className={cx('text')}>/월</span>
                 </div>
-                <button type="button" className={cx('tamo-layer cta__button')}>
+                <button type="button" className={cx('tamo-layer', 'cta__button')}>
                   바로구매
                 </button>
               </div>
@@ -70,35 +70,35 @@ const Arrive = () => {
           <article className={cx('sheet')} id="bottomUpSheet">
             <div className={cx('sheet__wrap')}>
               <div className={cx('info')}>
-                <div className="btn" />
-                  <strong className="option">수강권 선택 옵션</strong>
-                <div className="change-btn">
-                  <span className="text">변경</span>
+                <div className={cx('btn')} />
+                  <strong className={cx('option')}>수강권 선택 옵션</strong>
+                <div className={cx('change-btn')}>
+                  <span className={cx('text')}>변경</span>
                 </div>
-                <div className="textwrap">
-                  <span className="discount">추천</span>
-                  <span className="name bottom-up__title">
+                <div className={cx('textwrap')}>
+                  <span className={cx('discount')}>추천</span>
+                  <span className={cx('name', 'bottom-up__title')}>
                     365 올패스 Global 12 + 2 개월
                   </span>
                 </div>
                 <p>
                   <strong>
-                    <span className="minutes">20</span>분 / 주
-                    <span className="count__per__week">2</span>회
+                    <span className={cx('minutes')}>20</span>분 / 주
+                    <span className={cx('count__per__week')}>2</span>회
                   </strong>
-                  <span className="all">
-                    총<span className="lesson_cnt">98</span>회
+                  <span className={cx('all')}>
+                    총<span className={cx('lesson_cnt')}>98</span>회
                   </span>
                 </p>
               </div>
-              <div className="price-wrap">
+              <div className={cx('price-wrap')}>
                 <strong>할인 적용 가격</strong>
-                <span className="price-area">
-                  <span className="price">40,000원</span>/월
+                <span className={cx('price-area')}>
+                  <span className={cx('price')}>40,000원</span>/월
                 </span>
               </div>
             </div>
-            <div className="dim" />
+            <div className={cx('dim')} />
           </article>
         </section>
       </div>
